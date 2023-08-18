5d26c6ccc8008f977effec4bd9b51b77270a46cd
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-19 00:03:19 +0530

    functionality - generate random color and function to create rgb to hex color

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function rgbToHex(r, g, b){
-    const red = Math.min()
+    const red = Math.min(255, Math.max(0, r));
+    const green = Math.min(255, Math.max(0, g));
+    const blue = Math.min(255, Math.max(0, b));
+
+    const hexRed = red.toString(16).padStart(2, '0');
+    const hexGreen = green.toString(16).padStart(2, '0');
+    const hexBlue = blue.toString(16).padStart(2, '0');
+
+    return `#${hexRed}${hexGreen}${hexBlue}`;
 }
+
+function generateRandomColor(){
+    const r = Math.floor(Math.random() *256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const rgbColor =  `rgb(${r}, ${g}, ${b})`;
+    return rgbColor;
+}
+
+
+module.exports = {
+    rgbToHex,
+    generateRandomColor
+};
